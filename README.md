f9d44b264d6995c357af58a4673bd5a79ee4ddcb
Shubham Panchal <shubhampanchal9773@gmail.com>
2020-06-22 13:43:37 +0530

    Added multithreading for interactive-shell and progress report
    Shifted the generating pdf process into a different thread so as to keep the main thread interactive
    Added progress reports for the user - amount of work done
    Multithreading is done w.r.t deamon state i.e. thread generating pdf will die if application terminates
    Added some examples and made script more readable
    Added check for existince of output-directory

--- README.md
+++ README.md
@@ -6,11 +6,14 @@ You can find the attached python script `img2pdf.py`
 
 Let's say you have a bunch of images in `/home/user/Pictures/lecture-slides/` and you want to generate a pdf out of these files say `/home/user/Documents/lecture-slides/slides.pdf` then you can run the script as follows:
 
-`python3 img2pdf.py /home/user/Pictures/lecture-slides/ /home/user/Documents/lecture-slides/slides.pdf` or
+```python3 img2pdf.py /home/user/Pictures/lecture-slides/ /home/user/Documents/lecture-slides/slides.pdf```
 
-`./img2pdf.py /home/user/Pictures/lecture-slides/ /home/user/Documents/lecture-slides/slides.pdf`
+# Dependencies
+The script depends on the following packages/modules: "fpdf, PIL, os, sys, glob, threading"  
+All the modules are self-bundled with python3 except fpdf which you can install as follows:
+```sudo apt install python3-pip```
+```pip3 install fpdf```
 
-Make sure to give execution permission to script. You can do this using the command `sudo chmod +x img2pdf.py`
 
 # Advanced Options
 In case if you want to restrict only some of the images to be a part of pdf e.g. `*.jpg` and `.*png` only then you can edit the script to just keep those extensions
--- img2pdf.py
+++ img2pdf.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-
 # author: Shubham Panchal(http://shubhampanchal.herokuapp.com)
 # This small python script converts a set of images into a pdf file
 # arranging images in lexicographical-order of names
@@ -15,55 +13,26 @@ from PIL import Image
 import os
 import sys
 import glob
+import threading
+
+# prints the progress report to console
+def print_progress_report(percentage):
+	"""Prints the report to console"""
+	# print the work-done in percentage
+	print("\rProgress: [", end='')
+	# print percentage many #'s
+	for i in range(percentage):
+		print("#", end='')
+	# print (100 - percentage) many -'s
+	for i in range(100 - percentage):
+		print('-', end='')
+	# print the closing bracker
+	print("] {}%".format(percentage), end='')
 
 
-# define the process_directory procedure
-def process_directory(image_directory, output_path):
-	"""Takes a directory path and output file path and writes images as pdf to file"""
-	# check if directory exist
-	if not os.path.isdir(image_directory):
-		print("{} doesn't exist".format(image_directory))
-	# check if file ends with pdf
-	elif not output_path.endswith(".pdf"):
-		print("{} doesn't represent a path to a pdf file".format(output_path))
-	# check if output file already exist
-	elif os.path.isfile(output_path):
-		print("{} already exist".format(output_path))
-		# prompt user wheter to overwrite data
-		choice = input("Do you want to overwrite data(yes/no)? ")
-		# take decision based on user's choice
-		if choice.lower() != "yes":
-			# user opted for not to overwrite the data
-			print("Overwriting aborted by user")
-			# return since user aborted the process
-			return
-
-	# give a simple message to user
-	print("Working on your request...")
+# generates a pdf in background thread
+def generate_pdf(image_path_list, output_path):
 	try:
-		# add extensions here
-		extensions = ('*.jpg','*.png','*.gif')
-		# store the list of image-path
-		image_path_list = []
-		# fetch all image with given extension from user's directory
-		for extension in extensions:
-			image_path_list.extend(glob.glob(os.path.join(image_directory, extension)))
-		# sort the files w.r.t file names
-		image_path_list.sort()
-		# give user a message
-		print("Following files are going to be compressed to a pdf in same order:")
-		# print list to console
-		print(image_path_list)
-		# prompt user
-		choice = input("Are you sure to continue(yes/no)? ")
-		# take decision based on user's choice
-		if choice.lower() != "yes":
-			# user opted for not to overwrite the data
-			print("Process aborted by user")
-			# return since user aborted the process
-			return
-		# writing to pdf
-		print("Generating {}...".format(output_path))
 		# get the max width and max height
 		max_width = 0
 		max_height = 0
@@ -79,31 +48,137 @@ def process_directory(image_directory, output_path):
 			max_height = max(max_height, height)
 		# margin of 10pt
 		margin = 10
+		# get the number of images to process
+		total = len(image_path_list)
+		# get the number of images processed
+		processed = 0
+		# print the zero-progress report
+		print_progress_report(0)
 		# create an instance of FPDF
 		pdf = FPDF(unit="pt", format=[max_width + 2*margin, max_height + 2*margin])
 		# for each image add it to pdf
 		for imagePath in image_path_list:
+			# compute the percentage of work done
+			percentage = int(100*processed/total) 
+			# print the progress report
+			print_progress_report(percentage)
 			# add a new page to pdf
 			pdf.add_page()
 			# add image to pdf
 			pdf.image(imagePath, margin, margin)
+			# increase the number of images processed by 1
+			processed += 1
 		# write the pdf to system
 		pdf.output(output_path, "F")
-		# give a message to user
+		# print the percentage of total work-done
+		print_progress_report(100)
+		# leave a simple next line
+		print('')
+		# give a simple message to user
 		print("Request completed!")
+	# catch exception (if any)
+	except Exception:
+		print("\nOops, something went wrong )-:")
+
+
+# define the process_directory procedure
+def process_directory(image_directory, output_path):
+	"""Takes a directory path and output file path and writes images as pdf to file"""
+	# check if directory exist
+	if not os.path.isdir(image_directory):
+		# give a message to user
+		print("{} doesn't exist".format(image_directory))
+		# return from process
+		return
+	# check if file ends with pdf
+	elif not output_path.endswith(".pdf"):
+		# give a message to user
+		print("{} doesn't represent a path to a pdf file".format(output_path))
+		# return from the process
+		return
+	# check if output file already exist
+	elif os.path.isfile(output_path):
+		# give a message to user
+		print("{} already exist".format(output_path))
+		# prompt user wheter to overwrite data
+		choice = input("Do you want to overwrite data(yes/no)? ")
+		# take decision based on user's choice
+		if choice.lower() != "yes":
+			# user opted for not to overwrite the data
+			print("Overwriting aborted by user")
+			# return since user aborted the process
+			return
+
+
+	# get the directory of the output_path
+	output_dir = os.path.dirname(os.path.realpath(output_path))
+	# check if this represents a valid directory on os
+	if not os.path.isdir(output_dir):
+		# give a message to user
+		print("{} doesn't exist".format(output_dir))
+		# return from the process
+		return
+
+
+	# give a simple message to user
+	print("Collecting files image files in {}".format(image_directory))
+	try:
+		# add extensions here
+		extensions = ('*.jpg','*.png','*.gif')
+		# store the list of image-path
+		image_path_list = []
+		# fetch all image with given extension from user's directory
+		for extension in extensions:
+			image_path_list.extend(glob.glob(os.path.join(image_directory, extension)))
+		# sort the files w.r.t file names
+		image_path_list.sort()
+		# check if there exist some-file
+		if len(image_path_list) == 0:
+			# give a message to user
+			print("Sorry, we couldn't find any file in {} for generating a pdf!".format(image_directory))
+			# return from this point
+			return
+		# give user a message
+		print("Following files are going to be compressed to a pdf in same order:")
+		# print list to console
+		print(image_path_list)
+		# prompt user
+		choice = input("Are you sure to continue(yes/no)? ")
+		# take decision based on user's choice
+		if choice.lower() != "yes":
+			# user opted for not to overwrite the data
+			print("Process aborted by user")
+			# return since user aborted the process
+			return
+		# writing to pdf
+		print("Generating {}...".format(output_path))
+		# create a thread to run process
+		thread1  = threading.Thread(target=generate_pdf, args=(image_path_list, output_path,))
+		# set the daemon for thread1 to true
+		thread1.daemon = True
+		# start thread-1
+		thread1.start()
+		# wait until thread1 is finished
+		thread1.join()
 	except Exception:
 		# give an error message to user
-		print("Oops, something went wrong ):")
+		print("\nOops, something went wrong ):")
+
+
+# shows the usage of script to user
+def show_usage():
+	print("Usage:")
+	print("To generate a pdf `output_pdf_path` from images in `source_dir` use the following command:")
+	print("python3 img2pdf.py source_dir output_pdf_path")
+	print("Dependencies: pip3 install fpdf")
 
 # define main function - works with command line argumsnt
 def main():
 	"""Accepts the source directory containing images and output pdf path and 
 		converts images into a pdf sorted by name"""
 	if len(sys.argv) != 3:
-		print("Usage:")
-		print("python3 source_dir output_pdf_path")
-		print("or")
-		print("./img2pdf source_dir output_pdf_path")
+		# show usage to user
+		show_usage()
 	else:
 		process_directory(sys.argv[1], sys.argv[2])
 
